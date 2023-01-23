e321335fe7c1e54031eb17d696546d0e185643bf
Bronwen27 <118174027+Bronwen27@users.noreply.github.com>
2023-01-23 11:51:49 +0000

    added highscores talley

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -16,10 +16,23 @@ for (let i = 0; i < 10; i++) {
 */
 
 function printHighScore() {
+ let highScores =   JSON.parse(localStorage.getItem("highscores")) || [];
+ highScores.sort(function(a, b){
+  return b.score - a.score;
+ })
 
+ highScores.forEach(function(score) {
+  let li = document.createElement("li");
+  li.textContent = `${score.initials} - ${score.score}`
+
+  let ol = document.getElementById("highscores");
+  ol.appendChild(li);
+});
 }
 
 function clearHighScore() {
+localStorage.removeItem("highscores");
+window.location.reload();
 
 }
 
