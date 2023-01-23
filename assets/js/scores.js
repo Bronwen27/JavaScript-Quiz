abcba09ae348ad25620aca49249eab95618b6973
Bronwen27 <118174027+Bronwen27@users.noreply.github.com>
2023-01-23 13:40:36 +0000

    added comment to highscores

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,19 +1,5 @@
-/*
-let scores = [];
-Data.forEach(entry => {
-    entry.scoreHistory.forEach(score => {
-      scores.push({score:score, player:entry.username});
-    });
-});
 
-
-scores.sort((a, b) => b.score - a.score);
-
-
-for (let i = 0; i < 10; i++) {
-    console.log(scores[i]);
-}
-*/
+// Highscore chart 
 
 function printHighScore() {
  let highScores =   JSON.parse(localStorage.getItem("highscores")) || [];
